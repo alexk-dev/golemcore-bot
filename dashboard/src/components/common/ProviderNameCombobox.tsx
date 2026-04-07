c5c102842c60d85bbe0c9c3ddf7fb4f0d3ae74e1
Alex Kuleshov <alex@kuleshov.tech>
2026-04-07 13:36:57 -0400

    fix: unblock dashboard ui ci

--- dashboard/src/components/common/ProviderNameCombobox.tsx
+++ dashboard/src/components/common/ProviderNameCombobox.tsx
@@ -1,7 +1,7 @@
-import { type ReactElement } from 'react';
+import type { ReactElement } from 'react';
 import { AutocompleteCombobox, type AutocompleteComboboxProps } from './AutocompleteCombobox';
 
-export interface ProviderNameComboboxProps extends Omit<AutocompleteComboboxProps, 'emptyState'> {}
+export type ProviderNameComboboxProps = Omit<AutocompleteComboboxProps, 'emptyState'>;
 
 export function ProviderNameCombobox(props: ProviderNameComboboxProps): ReactElement {
   return <AutocompleteCombobox {...props} />;
